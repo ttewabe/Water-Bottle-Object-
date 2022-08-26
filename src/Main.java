6fed724b1b27b84c83be932c9707a620de6cabc0
Tewabe Tadege <ethiohabesha412@gmail.com>
2022-08-26 15:53:44 -0700

    Water-Bottle

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+class Main {
+    public static void main(String[] args) {
+
+        WaterBottle bottleOne = new WaterBottle();
+
+        bottleOne.check();
+        bottleOne.drink();
+        bottleOne.check();
+        WaterBottle bottleTwo= new WaterBottle();
+        bottleTwo.drink();
+        bottleTwo.drink();
+        bottleTwo.check();
+    }
+}
